9ff7ea97057f16cdec88a836f73dbce70e0a57eb
lixiaoniu <lixiaoniu@edaijia-inc.cn>
2017-10-19 18:50:31 +0800

    文件上传2

--- FirstWeb/src/fileupload/UploadServlet.java
+++ FirstWeb/src/fileupload/UploadServlet.java
@@ -1,12 +1,19 @@
 package fileupload;
 
+import com.sun.prism.shader.FillEllipse_Color_AlphaTest_Loader;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
 import javax.print.attribute.standard.RequestingUserName;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import java.io.*;
+import java.util.List;
 
 @WebServlet(name = "UploadServlet",urlPatterns = "/uploadServlet")
 public class UploadServlet extends HttpServlet {
@@ -16,5 +23,61 @@ public class UploadServlet extends HttpServlet {
 //        String file = req.getParameter("file");
 //        System.out.println(desc);
 //        System.out.println(file);
+        req.setCharacterEncoding("UTF-8");
+        //1.得到FileItem集合
+        DiskFileItemFactory factory = new DiskFileItemFactory();
+        //500k，临界值，超出这个临界值文件会被保存在临时目录中
+        factory.setSizeThreshold(1024 * 500);
+        //创建文件临时解析的目录
+        File temporyFile = new File("c:\\temporyFile");
+        factory.setRepository(temporyFile);
+        ServletFileUpload fileUpload = new ServletFileUpload(factory);
+        //请求消息实体内容的最大允许大小,也就是上传文件的最大容量
+        fileUpload.setSizeMax(1024 * 1024 * 5);//5M
+        fileUpload.setHeaderEncoding("UTF-8");//防止文件名乱码
+        List<FileItem> fileItems = null;
+        try {
+            fileItems  = fileUpload.parseRequest(req);
+        } catch (FileUploadException e) {
+            e.printStackTrace();
+        }
+        //2.遍历items
+        if (fileItems != null && fileItems.size() >  0) {
+            for (FileItem fileItem : fileItems) {
+                //表单域
+                if (fileItem.isFormField()) {
+                    //表单域名称
+                    String name = fileItem.getFieldName();
+                    //表单域值
+                    String string = fileItem.getString();
+                    System.out.println(name+"----"+string);
+                } else {
+                    //文件
+                    String fieldName = fileItem.getFieldName();
+                    String filename = fileItem.getName();
+                    String contentType = fileItem.getContentType();
+                    //文件大小，字节
+                    long size = fileItem.getSize();
+                    System.out.println(fieldName+"---"+filename+"---"+contentType+"---"+size);
+                    InputStream in = fileItem.getInputStream();
+                    byte[] buffer = new byte[1024];
+                    int len;
+                    filename = "c:\\files\\"+filename;
+                    System.out.println(filename);
+                    OutputStream out = new FileOutputStream(filename);
+                    while ((len = in.read(buffer)) != -1) {
+                        out.write(buffer,0,len);
+                    }
+                    in.close();
+                    out.close();
+                }
+            }
+        }
     }
 }
+/*
+* 文本文件
+* file---新建文本文档.txt---text/plain---25185
+* 图片
+* file---QQ图片20171017164804.jpg---image/jpeg---58714
+* */
--- FirstWeb/web/fileupload/upload.jsp
+++ FirstWeb/web/fileupload/upload.jsp
@@ -9,12 +9,35 @@
 <html>
 <head>
     <title>文件上传</title>
+    <script type="text/javascript" src="${pageContext.servletContext.contextPath}/scripts/jquery-3.2.1.min.js">
+
+    </script>
+    <script>
+        var count = 2;
+        $(function () {
+            $(".addFile").click(function () {
+                $("#br").before(" File"+
+                    count+": <input type='file' name='file+" +
+                    count +"+'/><br>Desc"+
+                    count+": <input type='text' name='desc" +
+                    count+" '/><input type='submit' value='上传'>" +
+                    " <button class='addFile+"+count+"' type='button'>Add</button>\n" +
+                    "        <button class='deleteFile+"+count+"' type='button'>Delete</button><br><br>");
+                count ++;
+            });
+        });
+    </script>
 </head>
 <body>
 <form action="/uploadServlet" method="post" enctype="multipart/form-data">
-    File :<input type="file" name="file"><br>
-    <input type="text" name="desc">
+    <span id="part">
+    File1: <input type="file" name="file"><br>
+    Desc1: <input type="text" name="desc">
     <input type="submit" value="上传">
+    <button class="addFile" type="button">Add</button>
+        <button class="deleteFile">Delete</button><br><br>
+    <br id="br">
+    </span>
 </form>
 </body>
 </html>
